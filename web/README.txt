37fe642158f36cf4146a9384d3f62cca1fbf6462
Yusuf Kucukugurlu <yusuf.kucukugurlu@gmail.com>
2026-04-17 15:52:27 +0300

    fix: rebuild web with --base-href=/web/ and updated prod API URL

--- web/README.txt
+++
@@ -1,5 +0,0 @@
-Flutter web build çıktısı buraya kopyalanacak.
-
-Komut:
-  flutter build web --dart-define=FLAVOR=prod --dart-define=BASE_URL=https://onlysocialsports-api.vercel.app/api --release
-  Copy-Item -Recurse -Force "build\web\*" "..\onlysocialsports\web\"
--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "366921394" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3167626631" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
